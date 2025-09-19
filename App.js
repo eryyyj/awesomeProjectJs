3d8f0028d4c121dcc0721d6a196748c9e3853374
Ery <pisalboneryjay@gmail.com>
2025-09-19 10:43:39 +0800

    new file:   .idea/.gitignore 	new file:   .idea/awesomeProjectJs.iml 	new file:   .idea/caches/deviceStreaming.xml 	new file:   .idea/deviceManager.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   .vscode/settings.json 	modified:   App.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals, 
+      { text: enteredGoalText, key: Math.random().toString() }
+    ]);
+    setEnteredGoalText('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Your Course Goal'
+          onChangeText={goalInputHandler}
+          value={enteredGoalText} 
+        />
+        <Button 
+          title='Add Goal'
+          onPress={addGoalHandler}
+        />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+        {courseGoals.map((item) => (
+          <Text key={item.key}>{item.text}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: '#cccccc',
+    width: '70%',
+    padding: 13,
+  },
+  goalsContainer: {
+    flex: 3,
+  }
 });
